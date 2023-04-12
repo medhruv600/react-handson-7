b828ed84ca5aa5d664306caec78878cc94b7994c
medhruv600 <medhruv600@gmail.com>
2023-04-12 17:20:27 +0545

    minor fixes!

--- src/Component/TableRow.js
+++ src/Component/TableRow.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const TableRow = ({student,student:{id,Name,Age,Course, Batch,},}) => {
+const TableRow = ({ student, student: { id, Name, Age, Course, Batch } }) => {
   return (
     <>
-    <td>{Name}</td>
-    <td className='algn'>{Age}</td>
-    <td className='algn'>{Course}</td>
-    <td className='algn'>{Batch}</td>
-    <td>
-        <Link to='/EditDetails' state={{id}}>Edit</Link>
-    </td>
+      <td className="t-head">{Name}</td>
+      <td className="t-head">{Age}</td>
+      <td className="t-head">{Course}</td>
+      <td className="t-head">{Batch}</td>
+      <td>
+        <Link to="/EditDetails" state={{ id }}>
+          Edit
+        </Link>
+      </td>
     </>
-  )
-}
+  );
+};
 
-export default TableRow
+export default TableRow;
